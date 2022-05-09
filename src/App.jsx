18358433fd4f1c6de00baf576c8a4e4d31899b2e
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-05-09 16:31:49 -0300

    Card 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import Primeiro from "./components/basicos/Primeiro";
 import Componente from "./components/basicos/Componente";
 import Fragmento from "./components/basicos/Fragmento";
@@ -7,26 +8,28 @@ import Card from "./components/layout/Card";
 
 const App = () => {
 	return (
-		<div id="app">
-      
+		<div className="App">
 			<h1>Fundamentos React</h1>
 
-			<Card titulo="#04 - Desafio Aleatório">
-				<Aleatorio max={10} min={1} />
-			</Card>
+			<div className="Cards">
 
-			<Card titulo="#03 - Fragmento">
-				<Fragmento></Fragmento>
-			</Card>
+				<Card titulo="#04 - Desafio Aleatório">
+					<Aleatorio max={10} min={1} />
+				</Card>
 
-			<Card titulo="#02 - Com Parametros">
-				<Componente titulo="Notas" aluno="Hugo" nota={6.8} />
-			</Card>
+				<Card titulo="#03 - Fragmento">
+					<Fragmento></Fragmento>
+				</Card>
 
-			<Card titulo="#01 - Primeiro Componente">
-				<Primeiro></Primeiro>
-			</Card>
+				<Card titulo="#02 - Com Parametros">
+					<Componente titulo="Notas" aluno="Hugo" nota={6.8} />
+				</Card>
 
+				<Card titulo="#01 - Primeiro Componente">
+					<Primeiro></Primeiro>
+				</Card>
+
+			</div>
 		</div>
 	);
 };
